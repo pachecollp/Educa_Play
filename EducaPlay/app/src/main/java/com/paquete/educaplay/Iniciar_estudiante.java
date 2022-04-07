ddb8ad33c4c327855b955448cfb629b9a98de8e7
angie1103 <anorena@ucundinamarca.edu.co>
2022-04-06 19:59:18 -0500

    Interfaces1

---
+++ EducaPlay/app/src/main/java/com/paquete/educaplay/Iniciar_estudiante.java
@@ -0,0 +1,14 @@
+package com.paquete.educaplay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Iniciar_estudiante extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iniciar_estudiante);
+    }
+}
---
+++ EducaPlay/app/src/main/java/com/paquete/educaplay/calificaciones_estudiantes.java
@@ -0,0 +1,14 @@
+package com.paquete.educaplay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class calificaciones_estudiantes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calificaciones_estudiantes);
+    }
+}
