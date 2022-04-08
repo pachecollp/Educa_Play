37229f7ab4633353ad8c66e26a052e2e9d1b8350
Jasegamo <jsebasgmora@gmail.com>
2022-04-08 12:27:22 -0500

    hacer validación registro usuario
    me la puede es re mamar si quiero escribir validasion con S

--- EducaPlay/app/src/main/java/com/paquete/educaplay/Iniciar_estudiante.java
+++ EducaPlay/app/src/main/java/com/paquete/educaplay/Iniciar_estudiante.java
@@ -5,13 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Iniciar_estudiante extends AppCompatActivity {
+    EditText usuario, contraseña;
+    Button iniciar_sesion;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iniciar_estudiante);
+        usuario = (EditText) findViewById(R.id.usuario_estudiante1);
+        contraseña = (EditText) findViewById(R.id.contraseña_estudiante1);
+        iniciar_sesion = (Button) findViewById(R.id.btniniciarestudi);
+        iniciar_sesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //enviar(nombreusuario);
+               // new Iniciar_estudiante.checkLogin().execute("");
+                finish();
+            }
+        });
     }
 
     public void iniciobuscar(View view){
--- EducaPlay/app/src/main/java/com/paquete/educaplay/registrar_estudiante.java
+++ EducaPlay/app/src/main/java/com/paquete/educaplay/registrar_estudiante.java
@@ -4,14 +4,45 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class registrar_estudiante extends AppCompatActivity {
+    EditText nom, ape, correo, cod, contr;
+    Button btnregistrarse;
+    String pasarusu;
+    View img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar_estudiante);
+        nom = (EditText) findViewById(R.id.nombre_estudiante);
+        ape = (EditText) findViewById(R.id.apellido_estudiante);
+        correo = (EditText) findViewById(R.id.correo_estudiante);
+        cod = (EditText) findViewById(R.id.codigo_estudiante);
+        contr = (EditText) findViewById(R.id.contraseña_estudiante);
+        btnregistrarse = (Button) findViewById(R.id.btnregistro);
+        img = (View)findViewById(R.id.img);
+        pasarusu = correo.getText().toString().trim();
+        btnregistrarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //agregarusuario();
+                Intent intent = new Intent(registrar_estudiante.this, Iniciar_estudiante.class);
+                //intent.putExtra("usu",pasarusu);
+                startActivity(intent);
+                finish();
+            }
+        });
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("nombre :", String.valueOf("2"));
+            }
+        });
     }
 
     public void registrarse(View view){
