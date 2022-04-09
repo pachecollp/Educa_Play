40da1dc448bf0d38c3907d23e47f1505736698cf
pachecollp <101750581+pachecollp@users.noreply.github.com>
2022-04-08 22:24:45 -0500

    inicio estudiantes

--- EducaPlay/app/src/main/java/com/paquete/educaplay/Iniciar_estudiante.java
+++ EducaPlay/app/src/main/java/com/paquete/educaplay/Iniciar_estudiante.java
@@ -3,6 +3,7 @@ package com.paquete.educaplay;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -30,6 +31,86 @@ public class Iniciar_estudiante extends AppCompatActivity {
         });
     }
 
+    public class checkLogin extends AsyncTask<String, String, String> {
+
+        String z = null;
+        Boolean isSuccess = false;
+        private registrar_estudiante ConnectionClass;
+
+        @Override
+        protected void onPreExecute(){
+
+        }
+
+        @Override
+        protected void onPostExecute(String s){
+            super.onPostExecute(s);
+        }
+
+        @Override
+        protected String doInBackground(String... strings){
+            con = conexionBD(ConnectionClass.nom.toString(),ConnectionClass.ape.toString(),ConnectionClass.corr.toString(),ConnectionClass.cod.toString(),ConnectionClass.contra.toString());
+            if(con == null){
+                Toast.makeText(iniciar_estudiante.this, "Revisa tu conexion", Toast.LENGTH_LONG).show();
+            }
+            else {
+                try {
+
+                    String sql = "SELECT * FROM estudiante WHERE usuario_estudiate = '" + usuario.getText() + "' AND contraseña_estudiante = '" + contraseña.getText() + "' ";
+                    nombreusuario = usuario.getText().toString();
+                    nom = ""+nombreusuario;
+                    Statement stmt = con.createStatement();
+                    ResultSet rs = stmt.executeQuery(sql);
+
+                    if (rs.next()) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(Iniciar_estudiante.this, "Inicio de sesión exitoso", Toast.LENGTH_LONG).show();
+
+                            }
+                        });
+                        z = "Success";
+                        Intent intent = new Intent(Iniciar_estudiante.this, buscar_estudiante.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(Iniciar_estudiante.this, "Revisa tu usuario o contraseña", Toast.LENGTH_LONG).show();
+                                Intent intent = new Intent(Iniciar_estudiante.this, Iniciar_estudiante.class);
+                                startActivity(intent);
+                            }
+                        });
+                        usuario.setText("");
+                        contraseña.setText("");
+                    }
+
+                } catch (Exception e) {
+                    isSuccess = false;
+                    Log.e("SQL Error :", e.getMessage());
+                }
+            }
+            return z;
+        }
+    }
+
+    @SuppressLint("NewApi")
+    public Connection conexionBD(String nom, String ape, String corr, String cod, String cotra){
+        Connection conexion = null;
+        try {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+            Class.forName("net.sourceforge.jtds.jdbc.Driver").newInstance();
+            conexion = DriverManager.getConnection("jdbc:jtds:sqlserver://gutgara.ddns.net;databaseName=EducaPlay;user=gutgara;password=VAuX2v_1xx0_T9w");
+
+        }catch (Exception e){
+            Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
+
+        }
+        return conexion;
+    }
     public void iniciobuscar(View view){
         Intent bus = new Intent(this, buscar_estudiante.class);
         startActivity(bus);
