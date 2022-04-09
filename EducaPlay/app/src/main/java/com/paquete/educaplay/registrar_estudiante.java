c7d402e3b84712ff0cb40f831217e0fdb5f24d0d
sebas9719 <101749747+sebas9719@users.noreply.github.com>
2022-04-08 21:54:20 -0500

    registro estudientes
    se realizo el regsitro conexion

--- EducaPlay/app/src/main/java/com/paquete/educaplay/registrar_estudiante.java
+++ EducaPlay/app/src/main/java/com/paquete/educaplay/registrar_estudiante.java
@@ -4,10 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLDataException;
+import java.sql.SQLException;
 
 public class registrar_estudiante extends AppCompatActivity {
     EditText nom, ape, correo, cod, contr;
@@ -44,6 +52,38 @@ public class registrar_estudiante extends AppCompatActivity {
             }
         });
     }
+    public Connection conexionBD(){
+        Connection conexion = null;
+        try {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+            Class.forName("net.sourceforge.jtds.jdbc.Driver").newInstance();
+            conexion = DriverManager.getConnection("jdbc:jtds:sqlserver://gutgara.ddns.net;databaseName=EducaPlay;user=gutgara;password=VAuX2v_1xx0_T9w");
+
+        }catch (Exception e){
+            Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
+
+        }
+        return conexion;
+    }
+    public void agregarusuario(){
+        try{
+            PreparedStatement pst = conexionBD().prepareStatement("insert into estudiante values(?,?,?,?,?)");
+            pst.setString(1,nom.getText().toString());
+            pst.setString(2,ape.getText().toString());
+            pst.setString(3,correo.getText().toString());
+            pst.setString(4,cod.getText().toString());
+            pst.setString(6,contr.getText().toString());
+            pst.executeUpdate();
+            Toast.makeText(getApplicationContext(),"REGISTRO AGREGADO CORRECTAMENTE",Toast.LENGTH_SHORT).show();
+        }catch (SQLDataException e){
+            Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+
 
     public void registrarse(View view){
         Intent inic = new Intent(this,Iniciar_estudiante.class);
